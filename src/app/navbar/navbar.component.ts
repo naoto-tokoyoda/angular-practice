67fdd2a78ac26bf6944ef1f2845856231f05b09a
Naoto Tokoyoda <tkyd89@gmail.com>
2022-08-16 18:48:00 -0700

    updated

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  //this selector will be added into html file where you want
+  selector: 'app-navbar',
+  templateUrl:'./navbar.component.html'
+})
+//export this file
+export class NavbarComponent{
+
+}
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  //this selector will be added into html file where you want
+  selector: 'app-sidebar',
+  templateUrl:'./sidebar.component.html'
+})
+//export this file
+export class SidebarComponent{
+
+}
